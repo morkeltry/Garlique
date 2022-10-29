895a6a682bd316b0deade8311255a1f0d6eddcc3
Hugo Montenegro <h@hugomontenegro.com>
2022-10-29 13:41:16 +0200

    simple signing server

--- SigningServer/app.py
+++ SigningServer/app.py
@@ -14,10 +14,23 @@ from flask import (
     redirect,
     request,
 )
+import json
 from werkzeug.exceptions import HTTPException
 import requests
 from ecdsa import SigningKey
+import ecdsa
 
+# # generate ecdsa key pair
+# signing_key = SigningKey.generate(curve=ecdsa.SECP256k1)
+# verify_key = signing_key.get_verifying_key()
+# SERVER_PRIVATE_KEY = signing_key.to_string().hex()
+# SERVER_PUBLIC_KEY = verify_key.to_string().hex()
+SERVER_PRIVATE_KEY = "badea878ddd6f34cd2d061f3a095f3dba6be10357d5c5ef0b7e5a64ced930809"
+SERVER_PUBLIC_KEY = "63439c944f4a2680cb9cd13a45341241fb6bb22c9df5a3028aae5766d589464ddd084ef3f8f873217c200b1a8d66e39f17eaeb7a9a0b02c27a6e181ee7a729e1"
+signing_key = SigningKey.from_string(bytes.fromhex(SERVER_PRIVATE_KEY), curve=ecdsa.SECP256k1)
+verify_key = signing_key.get_verifying_key()
+print(f"SERVER_PRIVATE_KEY: {SERVER_PRIVATE_KEY}")
+print(f"SERVER_PUBLIC_KEY: {SERVER_PUBLIC_KEY}")
 
 app = Flask(__name__)
 ETHERSCAN_API_KEY = "A5B6KPZ99R1DHF94EEAJMIUSW3DWJDQCM5"
@@ -39,7 +52,7 @@ def sign():
         @value
         @recipient
         @salt
-        @blockheight
+        @blocknumber
     returns:
         @signature
     """
@@ -49,27 +62,33 @@ def sign():
         value = data['value']
         recipient = data['recipient']
         salt = data['salt']
-        blockheight = data['blockheight']
+        blocknumber = data['blocknumber']
 
         blockexplorer_url = f"https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash={txhash}&apikey={ETHERSCAN_API_KEY}"
         r = requests.get(blockexplorer_url)
-        if r.status_code == 200:
-            # tx exists on blockchain
-            # TODO: further verification
+        if r.status_code != 200:
+            return make_response(jsonify({'error': 'Could not get transaction from blockexplorer'}), 400)
+        
+        if "result" not in r.json():
+            return make_response(jsonify({'error': 'tx not found'}), 400)
+        
+        result = r.json()["result"]
+        print(result)
+        # TODO: some validation logic that makes sense here
+        if result["blockNumber"] != blocknumber:
+            return make_response(jsonify({'error': 'tx not confirmed'}), 400)
 
-            # sign json object
-            message = {
-                "txhash": txhash,
-                "value": value,
-                "recipient": recipient,
-                "salt": salt,
-                "blockheight": blockheight
-            }
-            signature = sign_message(message)
-            return jsonify({"signature": signature})
-        else:
-            print(r.status_code)
-            return jsonify({"error": "tx not found"})
+
+        # sign json object
+        message = {
+            "txhash": txhash,
+            "value": value,
+            "recipient": recipient,
+            "salt": salt,
+            "blocknumber": blocknumber
+        }
+        signature = sign_message(message)
+        return jsonify({"signature": signature})
     else:
         return jsonify({"error": "invalid request"})
 
@@ -78,15 +97,15 @@ def sign_message(message):
     """
     Signs a message with the server's private key
     """
-    key = RSA.importKey(SERVER_PRIVATE_KEY)
-    signature = key.sign(message, '')
-    return signature
+    message = json.dumps(message, sort_keys=True)
+    signature = signing_key.sign(message.encode())
+    return signature.hex()
     
 
 if __name__ == '__main__':
     app.run(debug=True)
 
     # test with curl
-    # curl -H "Content-Type: application/json" -X POST -d '{"txhash":"0xadb037f6d8d2f31c39d3ac14ab2590956b74ddb8fbf9f0d74176ac3c43d709c0","value":"1000000000000000000","recipient":"0x2a1cb5fd815b0ec7628c2f70276b552c12921fd2032d11ca1423a939e51682eb","salt":"271721t ","blockheight":"111"}' http://127.0.0.1:5000/sign
+    # curl -H "Content-Type: application/json" -X POST -d '{"txhash":"0xadb037f6d8d2f31c39d3ac14ab2590956b74ddb8fbf9f0d74176ac3c43d709c0","value":"1000000000000000000","recipient":"0x2a1cb5fd815b0ec7628c2f70276b552c12921fd2032d11ca1423a939e51682eb","salt":"271721t ","blocknumber":"0xf0b04f"}' http://127.0.0.1:5000/sign
 
 
