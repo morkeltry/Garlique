cac72a7a8fdbd201d935576b6544c02d361ca0c3
dips <your@email.com>
2022-10-30 01:25:30 +0000

    npx hardhat node && npx hardhat run --network localhost scripts/deploy.js deploys contract locally

---
+++ constants/garlic_patch.mjs
@@ -0,0 +1,2 @@
+const garlicPatch = "0xa4d046a1868c056e805490795b782711E3e26c4A";
+export {garlicPatch};
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,26 +6,26 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+async function main(garlicPatch) {
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Garlique = await hre.ethers.getContractFactory("Garlique");
+  const garlique = await Garlique.deploy(garlicPatch);
 
-  await lock.deployed();
+  await garlique.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Deployed to ${garlique.address}`
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+import("../constants/garlic_patch.mjs")
+  .then (({garlicPatch}) => {    
+    console.log (`Deploying with server addy: ${garlicPatch}`);
+    // We recommend this pattern to be able to use async/await everywhere
+    // and properly handle errors.
+    main(garlicPatch).catch((error) => {
+      console.error(error);
+      process.exitCode = 1;
+    });
+})
